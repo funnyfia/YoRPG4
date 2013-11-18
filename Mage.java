acf8e6bca40a308b63034b3cb9d19d52ee579b80
rosyang <yang.xy9@gmail.com>
2013-11-17 20:27:55 -0500

    Update Mage.java

--- Mage.java
+++ Mage.java
@@ -5,34 +5,34 @@ public class Mage extends Character {
 
     //Default constructor
     public Mage( String name ) {
-	      super();
-	      _name = name;
-	      _HP = 175;
-	      _strength = 130;
-	      _defense = 30;
-	      _attack = .5
+	super();
+	_name = name;
+	_HP = 175;
+	_strength = 130;
+	_defense = 30;
+	_attack = .5
     }
 
     //Accessor
     public String getName() {
-	      return _name;
+	return _name;
     }
 
     //Prepares the Mage to perform a special attack
     public void specialize() {
-	      _defense = 20;
-	      _attack = .75;
+	_defense = 20;
+	_attack = .75;
     }
 
     //Prepares the Mage to perform a normal attack
     public void normalize() {
-	      _defense = 30;
-	      _attack = 0.5;
+	_defense = 30;
+	_attack = 0.5;
     }
 
     public boolean critical() {
-	      double chance = Math.random();
-	      return chance < .25;
+	double chance = Math.random();
+	return chance < .25;
     }
   
 }
